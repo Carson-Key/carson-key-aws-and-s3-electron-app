ae0a3be70ef1c158d111e6cd612049466aed7f8b
Carson-Key <keyc@oregonstate.edu>
2021-02-04 10:17:14 -0800

    add input label

--- src/App.js
+++ src/App.js
@@ -21,23 +21,24 @@ function App() {
   }, [])
 
   return (
-    <div className="bg-gray-800 w-screen h-screen top-0 absolute">
+    <div className="bg-gray-800 w-screen h-screen top-0 absolute text-white">
       <div className="flex justify-around">
-        <div className="mt-4 text-white">
+        <div className="mt-4">
           <div>
             <p className="font-bold">Current AWS Profile:</p><p className="ml-4">{" " + awsProfileReturned}</p>
           </div>
         </div>
         <div>
           <div className="mt-4 mx-3">
+            <p>Set AWS Profile Used</p>
             <input
               onChange={(event) => {setAwsProfile(event.target.value)}}
               placeholder={"AWS Profile"}
             />
-            <button className="bg-blue-400 text-white mx-5 px-3 py-1 rounded" onClick={handleSetAWSProfile}>submit</button>
+            <button className="bg-blue-400 mx-5 px-3 py-1 rounded" onClick={handleSetAWSProfile}>submit</button>
           </div>
           <div className="mt-4 mx-3">
-            <button className="bg-blue-400 text-white px-3 py-1 rounded" onClick={() => {ipcRenderer.send('getAWSProfile', awsProfile)}}>Profile</button>
+            <button className="bg-blue-400 px-3 py-1 rounded" onClick={() => {ipcRenderer.send('getAWSProfile', awsProfile)}}>Profile</button>
           </div>
         </div>
       </div>
