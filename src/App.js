a1950b9065c35cec93789fea06e2c24d395aaa01
Carson-Key <keyc@oregonstate.edu>
2021-02-04 09:46:09 -0800

    Add elements to show current aws profile

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const { ipcRenderer } = window.require('electron');
 
 function App() {
   const [awsProfile, setAwsProfile] = useState("")
+  const [awsProfileReturned, setAwsProfileReturned] = useState("defualt")
 
   const handleSetAWSProfile = () => {
     if (awsProfile !== "") {
@@ -14,21 +15,30 @@ function App() {
 
   useEffect(() => {
     ipcRenderer.on('getAWSProfile-reply', (event, arg) => {
-      console.log(arg)
+      setAwsProfileReturned(arg)
     })
   }, [])
 
   return (
-    <div>
-      <div className="mt-4 mx-3">
-        <input
-          onChange={(event) => {setAwsProfile(event.target.value)}}
-          placeholder={"AWS Profile"}
-        />
-        <button className="bg-blue-400 text-white mx-5 px-3 py-1 rounded" onClick={handleSetAWSProfile}>submit</button>
-      </div>
-      <div className="mt-4 mx-3">
-        <button className="bg-blue-400 text-white px-3 py-1 rounded" onClick={() => {ipcRenderer.send('getAWSProfile', awsProfile)}}>Profile</button>
+    <div className="bg-gray-800 w-screen h-screen top-0 absolute">
+      <div className="flex justify-around">
+        <div className="mt-4 text-white">
+          <div>
+            <p className="font-bold">Current AWS Profile:</p><p className="ml-4">{" " + awsProfileReturned}</p>
+          </div>
+        </div>
+        <div>
+          <div className="mt-4 mx-3">
+            <input
+              onChange={(event) => {setAwsProfile(event.target.value)}}
+              placeholder={"AWS Profile"}
+            />
+            <button className="bg-blue-400 text-white mx-5 px-3 py-1 rounded" onClick={handleSetAWSProfile}>submit</button>
+          </div>
+          <div className="mt-4 mx-3">
+            <button className="bg-blue-400 text-white px-3 py-1 rounded" onClick={() => {ipcRenderer.send('getAWSProfile', awsProfile)}}>Profile</button>
+          </div>
+        </div>
       </div>
     </div>
   );
