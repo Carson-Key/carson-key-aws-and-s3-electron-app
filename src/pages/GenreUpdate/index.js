f25834dc141621398b072c9d5fd867cb97b2437d
Carson-Key <keyc@oregonstate.edu>
2021-03-05 16:45:37 -0800

    Add frontend for updating genre

--- src/pages/GenreUpdate/index.js
+++ src/pages/GenreUpdate/index.js
@@ -2,28 +2,40 @@ import React, { useState } from 'react'
 
 const { ipcRenderer } = window.require('electron');
 
-function SetAWSProfile() {
-    const [awsProfile, setAwsProfile] = useState("")
+function UpdateGenre() {
+    const [oldGenre, setOldGenre] = useState("")
+    const [newGenre, setNewGenre] = useState("")
 
-    const handleSetAWSProfile = () => {
-        if (awsProfile !== "") {
-            ipcRenderer.send('setAWSCreds', awsProfile)
-            ipcRenderer.send('getAWSProfile')
+    const handleUpdateGenre = () => {
+        if (oldGenre !== "" && newGenre !== "") {
+            ipcRenderer.send('updateGenre', oldGenre, newGenre)
         }
     }
 
     return (
         <div className="mt-4 mx-3">
-            <p>Set AWS Profile Used</p>
-            <input
-                id="awsProfile"
-                className="text-black px-2 ml-4"
-                onChange={(event) => {setAwsProfile(event.target.value)}}
-                placeholder="AWS Profile"
-            />
-            <button className="bg-blue-400 mx-5 px-3 py-1 rounded" onClick={handleSetAWSProfile}>submit</button>
+            <p>Update Genre</p>
+            <div className="ml-4 mb-6">
+                <p>Old Genre Name</p>
+                <input
+                    id="oldGenreName"
+                    className="text-black px-2"
+                    onChange={(event) => {setOldGenre(event.target.value)}}
+                    placeholder="Old Genre"
+                />
+            </div>
+            <div className="ml-4 mb-6">
+                <p>New Genre Name</p>
+                <input
+                    id="newGenreName"
+                    className="text-black px-2"
+                    onChange={(event) => {setNewGenre(event.target.value)}}
+                    placeholder="New Genre"
+                />
+            </div>
+            <button className="bg-blue-400 mx-5 px-3 py-1 rounded" onClick={handleUpdateGenre}>submit</button>
         </div>
     )
 }
 
-export default SetAWSProfile
+export default UpdateGenre
