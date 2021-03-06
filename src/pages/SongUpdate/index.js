ceb869ae51a95a902fd356f127075b8e96b88f4f
Carson-Key <keyc@oregonstate.edu>
2021-03-05 18:31:39 -0800

    Add update song to frontend

--- src/pages/SongUpdate/index.js
+++ src/pages/SongUpdate/index.js
@@ -2,28 +2,40 @@ import React, { useState } from 'react'
 
 const { ipcRenderer } = window.require('electron');
 
-function SetAWSProfile() {
-    const [awsProfile, setAwsProfile] = useState("")
+function SongUpdate() {
+    const [oldSong, setOldSong] = useState("")
+    const [newSong, setNewSong] = useState("")
 
-    const handleSetAWSProfile = () => {
-        if (awsProfile !== "") {
-            ipcRenderer.send('setAWSCreds', awsProfile)
-            ipcRenderer.send('getAWSProfile')
+    const handleUpdateSong = () => {
+        if (oldSong !== "" && newSong !== "") {
+            ipcRenderer.send('updateSong', {oldSong, newSong})
         }
     }
 
     return (
         <div className="mt-4 mx-3">
-            <p>Set AWS Profile Used</p>
-            <input
-                id="awsProfile"
-                className="text-black px-2 ml-4"
-                onChange={(event) => {setAwsProfile(event.target.value)}}
-                placeholder="AWS Profile"
-            />
-            <button className="bg-blue-400 mx-5 px-3 py-1 rounded" onClick={handleSetAWSProfile}>submit</button>
+            <p>Update Album</p>
+            <div className="ml-4 mb-6">
+                <p>Old Song Name</p>
+                <input
+                    id="oldSongName"
+                    className="text-black px-2"
+                    onChange={(event) => {setOldSong(event.target.value)}}
+                    placeholder="Old Song"
+                />
+            </div>
+            <div className="ml-4 mb-6">
+                <p>New Song Name</p>
+                <input
+                    id="newSongName"
+                    className="text-black px-2"
+                    onChange={(event) => {setNewSong(event.target.value)}}
+                    placeholder="New Song"
+                />
+            </div>
+            <button className="bg-blue-400 mx-5 px-3 py-1 rounded" onClick={handleUpdateSong}>submit</button>
         </div>
     )
 }
 
-export default SetAWSProfile
+export default SongUpdate
